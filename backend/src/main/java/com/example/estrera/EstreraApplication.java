73073dd92d395ea29719323196c6624b61acbe26
LAB13_USER ON WS22 <L13Y12W22@LabHCI.net>
2026-02-07 11:37:58 +0800

    backend(setup): initial springboot setup

---
+++ backend/src/main/java/com/example/estrera/EstreraApplication.java
@@ -0,0 +1,13 @@
+package com.example.estrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstreraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstreraApplication.class, args);
+	}
+
+}
